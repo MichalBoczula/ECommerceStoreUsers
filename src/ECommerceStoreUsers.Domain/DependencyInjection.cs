83b13356661417a0d95975302d06a5bd24a68556
Michal  Boczula <38834900+MichalBoczula@users.noreply.github.com>
2026-05-19 20:54:19 +0200

    Add domain validation policies to DI registration

--- src/ECommerceStoreUsers.Domain/DependencyInjection.cs
+++ src/ECommerceStoreUsers.Domain/DependencyInjection.cs
@@ -1,4 +1,14 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using ECommerceStoreUsers.Domain.AggregatesModel.Customers;
+using ECommerceStoreUsers.Domain.AggregatesModel.Customers.Entities;
+using ECommerceStoreUsers.Domain.AggregatesModel.Customers.ValueObjects;
+using ECommerceStoreUsers.Domain.AggregatesModel.Employees;
+using ECommerceStoreUsers.Domain.Validation.Abstract;
+using ECommerceStoreUsers.Domain.Validation.Concrete.Policies.Customers.Addresses;
+using ECommerceStoreUsers.Domain.Validation.Concrete.Policies.Customers.Customers;
+using ECommerceStoreUsers.Domain.Validation.Concrete.Policies.Customers.Entities.CompanyDatas;
+using ECommerceStoreUsers.Domain.Validation.Concrete.Policies.Customers.Entities.IndividualDatas;
+using ECommerceStoreUsers.Domain.Validation.Concrete.Policies.Employees.Admins;
 
 namespace ECommerceStoreUsers.Domain
 {
@@ -7,6 +17,17 @@ namespace ECommerceStoreUsers.Domain
         public static IServiceCollection AddDomain(
            this IServiceCollection services)
         {
+            services.AddScoped<IValidationPolicy<Customer>, CustomerValidationPolicy>();
+            services.AddScoped<IValidationPolicyDescriptorProvider, CustomerValidationPolicy>();
+            services.AddScoped<IValidationPolicy<Address>, AddressValidationPolicy>();
+            services.AddScoped<IValidationPolicyDescriptorProvider, AddressValidationPolicy>();
+            services.AddScoped<IValidationPolicy<IndividualData>, IndividualDataValidationPolicy>();
+            services.AddScoped<IValidationPolicyDescriptorProvider, IndividualDataValidationPolicy>();
+            services.AddScoped<IValidationPolicy<CompanyData>, CompanyDataValidationPolicy>();
+            services.AddScoped<IValidationPolicyDescriptorProvider, CompanyDataValidationPolicy>();
+            services.AddScoped<IValidationPolicy<Admin>, AdminValidationPolicy>();
+            services.AddScoped<IValidationPolicyDescriptorProvider, AdminValidationPolicy>();
+
             return services;
         }
     }

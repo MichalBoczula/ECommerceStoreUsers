4e9b4f47e57b11b406c7aa35a040453578f5d79b
Michal  Boczula <38834900+MichalBoczula@users.noreply.github.com>
2026-05-23 00:56:44 +0200

    Add GUID validation steps to customer flow descriptors

--- src/ECommerceStoreUsers.Application/Descriptors/Customers/AddCompanyDescriptor.cs
+++ src/ECommerceStoreUsers.Application/Descriptors/Customers/AddCompanyDescriptor.cs
@@ -14,13 +14,28 @@ namespace ECommerceStoreUsers.Application.Descriptors.Customers
 
     internal sealed class AddCompanyDescriptor : FlowDescriberBase<AddCompany>
     {
-        [FlowStep(order: 1, bpmnId: "LoadCustomerProfile")]
+        [FlowStep(order: 1, bpmnId: "ValidateCustomerId")]
+        public async Task<ValidationResult> ValidateCustomerId(Guid customerId, IValidationPolicy<Guid> emptyGuidValidationPolicy)
+        {
+            return await emptyGuidValidationPolicy.Validate(customerId);
+        }
+
+        [FlowStep(order: 2, bpmnId: "IsCustomerIdValid")]
+        public void ThrowValidationExceptionIfCustomerIdInvalid(ValidationResult validationResult)
+        {
+            if (!validationResult.IsValid)
+            {
+                throw new ValidationException(validationResult);
+            }
+        }
+
+        [FlowStep(order: 3, bpmnId: "LoadCustomerProfile")]
         public async Task<Customer?> LoadCustomer(Guid customerId, ICustomerRepository customerRepository, CancellationToken cancellationToken)
         {
             return await customerRepository.GetByIdAsync(customerId, cancellationToken);
         }
 
-        [FlowStep(order: 2, bpmnId: "VerifyCustomerExists")]
+        [FlowStep(order: 4, bpmnId: "VerifyCustomerExists")]
         public void ThrowNotFoundExceptionIfCustomerMissing(Guid customerId, Customer? customer)
         {
             if (customer is null)
@@ -32,25 +47,25 @@ namespace ECommerceStoreUsers.Application.Descriptors.Customers
             }
         }
 
-        [FlowStep(order: 3, bpmnId: "MapAddress")]
+        [FlowStep(order: 5, bpmnId: "MapAddress")]
         public Address MapAddress(AddressRequestDto address)
         {
             return MappingConfig.MapAddress(address);
         }
 
-        [FlowStep(order: 4, bpmnId: "AddCompanyToCustomer")]
+        [FlowStep(order: 6, bpmnId: "AddCompanyToCustomer")]
         public void AddCompanyToCustomer(Customer customer, AddCompanyRequestDto request, Address billing, Address shipping)
         {
             customer.AddCompany(request.CompanyName, request.TaxId, billing, shipping);
         }
 
-        [FlowStep(order: 5, bpmnId: "ValidateCustomerAggregate")]
+        [FlowStep(order: 7, bpmnId: "ValidateCustomerAggregate")]
         public async Task<ValidationResult> ValidateCustomer(Customer customer, IValidationPolicy<Customer> customerValidationPolicy)
         {
             return await customerValidationPolicy.Validate(customer);
         }
 
-        [FlowStep(order: 6, bpmnId: "IsCustomerAggregateValid")]
+        [FlowStep(order: 8, bpmnId: "IsCustomerAggregateValid")]
         public void ThrowValidationExceptionIfCustomerInvalid(ValidationResult validationResult)
         {
             if (!validationResult.IsValid)
@@ -59,13 +74,13 @@ namespace ECommerceStoreUsers.Application.Descriptors.Customers
             }
         }
 
-        [FlowStep(order: 7, bpmnId: "SaveCustomer")]
+        [FlowStep(order: 9, bpmnId: "SaveCustomer")]
         public async Task<Customer> Save(Customer customer, ICustomerRepository customerRepository, CancellationToken cancellationToken)
         {
             return await customerRepository.UpdateCustomer(customer, cancellationToken);
         }
 
-        [FlowStep(order: 8, bpmnId: "MapCustomerResponse")]
+        [FlowStep(order: 10, bpmnId: "MapCustomerResponse")]
         public CustomerResponseDto MapToResponse(Customer customer)
         {
             return MappingConfig.MapToResponse(customer);
--- src/ECommerceStoreUsers.Application/Descriptors/Customers/UpdateCompanyDescriptor.cs
+++ src/ECommerceStoreUsers.Application/Descriptors/Customers/UpdateCompanyDescriptor.cs
@@ -15,13 +15,28 @@ namespace ECommerceStoreUsers.Application.Descriptors.Customers
 
     internal sealed class UpdateCompanyDescriptor : FlowDescriberBase<UpdateCompany>
     {
-        [FlowStep(order: 1, bpmnId: "LoadCustomerProfile")]
+        [FlowStep(order: 1, bpmnId: "ValidateCustomerId")]
+        public async Task<ValidationResult> ValidateCustomerId(Guid customerId, IValidationPolicy<Guid> emptyGuidValidationPolicy)
+        {
+            return await emptyGuidValidationPolicy.Validate(customerId);
+        }
+
+        [FlowStep(order: 2, bpmnId: "IsCustomerIdValid")]
+        public void ThrowValidationExceptionIfCustomerIdInvalid(ValidationResult validationResult)
+        {
+            if (!validationResult.IsValid)
+            {
+                throw new ValidationException(validationResult);
+            }
+        }
+
+        [FlowStep(order: 3, bpmnId: "LoadCustomerProfile")]
         public async Task<Customer?> LoadCustomer(Guid customerId, ICustomerRepository customerRepository, CancellationToken cancellationToken)
         {
             return await customerRepository.GetByIdAsync(customerId, cancellationToken);
         }
 
-        [FlowStep(order: 2, bpmnId: "VerifyCustomerExists")]
+        [FlowStep(order: 4, bpmnId: "VerifyCustomerExists")]
         public void ThrowNotFoundExceptionIfCustomerMissing(Guid customerId, Customer? customer)
         {
             if (customer is null)
@@ -30,19 +45,34 @@ namespace ECommerceStoreUsers.Application.Descriptors.Customers
             }
         }
 
-        [FlowStep(order: 3, bpmnId: "MapAddress")]
+        [FlowStep(order: 5, bpmnId: "ValidateCompanyId")]
+        public async Task<ValidationResult> ValidateCompanyId(Guid companyId, IValidationPolicy<Guid> emptyGuidValidationPolicy)
+        {
+            return await emptyGuidValidationPolicy.Validate(companyId);
+        }
+
+        [FlowStep(order: 6, bpmnId: "IsCompanyIdValid")]
+        public void ThrowValidationExceptionIfCompanyIdInvalid(ValidationResult validationResult)
+        {
+            if (!validationResult.IsValid)
+            {
+                throw new ValidationException(validationResult);
+            }
+        }
+
+        [FlowStep(order: 7, bpmnId: "MapAddress")]
         public Address MapAddress(AddressRequestDto address)
         {
             return MappingConfig.MapAddress(address);
         }
 
-        [FlowStep(order: 4, bpmnId: "LoadCompanyFromAggregate")]
+        [FlowStep(order: 8, bpmnId: "LoadCompanyFromAggregate")]
         public CompanyData? LoadCompany(Customer customer, Guid companyId)
         {
             return customer.Companies.FirstOrDefault(x => x.Id == companyId);
         }
 
-        [FlowStep(order: 5, bpmnId: "VerifyCompanyExists")]
+        [FlowStep(order: 9, bpmnId: "VerifyCompanyExists")]
         public void ThrowNotFoundExceptionIfCompanyMissing(Guid companyId, CompanyData? company)
         {
             if (company is null)
@@ -51,19 +81,19 @@ namespace ECommerceStoreUsers.Application.Descriptors.Customers
             }
         }
 
-        [FlowStep(order: 6, bpmnId: "UpdateCompanyInsideAggregate")]
+        [FlowStep(order: 10, bpmnId: "UpdateCompanyInsideAggregate")]
         public void UpdateCompanyInsideAggregate(CompanyData company, UpdateCompanyRequestDto request, Address billing, Address shipping)
         {
             company.UpdateCompanyDetails(request.TaxId, request.CompanyName, billing, shipping);
         }
 
-        [FlowStep(order: 7, bpmnId: "ValidateCustomerAggregate")]
+        [FlowStep(order: 11, bpmnId: "ValidateCustomerAggregate")]
         public async Task<ValidationResult> ValidateCustomer(Customer customer, IValidationPolicy<Customer> customerValidationPolicy)
         {
             return await customerValidationPolicy.Validate(customer);
         }
 
-        [FlowStep(order: 8, bpmnId: "IsCustomerAggregateValid")]
+        [FlowStep(order: 12, bpmnId: "IsCustomerAggregateValid")]
         public void ThrowValidationExceptionIfCustomerInvalid(ValidationResult validationResult)
         {
             if (!validationResult.IsValid)
@@ -72,13 +102,13 @@ namespace ECommerceStoreUsers.Application.Descriptors.Customers
             }
         }
 
-        [FlowStep(order: 9, bpmnId: "SaveCustomer")]
+        [FlowStep(order: 13, bpmnId: "SaveCustomer")]
         public async Task<Customer> Save(Customer customer, ICustomerRepository customerRepository, CancellationToken cancellationToken)
         {
             return await customerRepository.UpdateCustomer(customer, cancellationToken);
         }
 
-        [FlowStep(order: 10, bpmnId: "MapCustomerResponse")]
+        [FlowStep(order: 14, bpmnId: "MapCustomerResponse")]
         public CustomerResponseDto MapToResponse(Customer customer)
         {
             return MappingConfig.MapToResponse(customer);

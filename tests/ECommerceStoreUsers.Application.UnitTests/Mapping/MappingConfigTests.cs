c782c5bd095cf01a1589d2a67408684bed237e10
Michal Boczula <michal.boczula0101@gmail.com>
2026-05-23 18:45:22 +0200

    Added mapping tests layer

---
+++ tests/ECommerceStoreUsers.Application.UnitTests/Mapping/MappingConfigTests.cs
@@ -0,0 +1,11 @@
+﻿namespace ECommerceStoreUsers.Application.UnitTests.Mapping
+{
+    public class MappingConfigTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}

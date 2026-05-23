514f16c3bf0f026c0c3062c453a438ebd4ec515b
Michal Boczula <michal.boczula0101@gmail.com>
2026-05-23 18:57:53 +0200

    Addded infra mapping test layer

---
+++ tests/ECommerceStoreUsers.Infrastructure.UnitTests/Mapping/CustomerMappingTests.cs
@@ -0,0 +1,11 @@
+﻿namespace ECommerceStoreUsers.Infrastructure.UnitTests.Mapping
+{
+    public class CustomerMappingTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}

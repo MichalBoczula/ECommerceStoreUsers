001d0ba973ff1215237544b38406a1202154bf0f
Michal  Boczula <38834900+MichalBoczula@users.noreply.github.com>
2026-05-28 01:02:26 +0200

    Fix customer benchmark namespace

---
+++ tests/ECommerceStoreUsers.Performance.BenchmarkTests/Employees/Domain/Customers/CustomersValidationPolicyBenchmarks.cs
@@ -0,0 +1,67 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Order;
+using ECommerceStoreUsers.Domain.AggregatesModel.Customers;
+using ECommerceStoreUsers.Domain.Validation.Abstract;
+using ECommerceStoreUsers.Domain.Validation.Concrete.Policies.Customers;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ECommerceStoreUsers.Performance.BenchmarkTests.Customers.Domain
+{
+    [MemoryDiagnoser]
+    [Orderer(SummaryOrderPolicy.FastestToSlowest)]
+    [RankColumn]
+    public class CustomersValidationPolicyBenchmarks
+    {
+        private IServiceProvider _serviceProvider = null!;
+        private IServiceScope _serviceScope = null!;
+        private IValidationPolicy<Customer> _policy = null!;
+        private Customer _validEntity = null!;
+        private Customer _invalidExternalIdEntity = null!;
+        private Customer _allInvalidEntity = null!;
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            var services = new ServiceCollection();
+
+            services.AddScoped<IValidationPolicy<Customer>, CustomerValidationPolicy>();
+
+            _serviceProvider = services.BuildServiceProvider();
+            _serviceScope = _serviceProvider.CreateScope();
+
+            _policy = _serviceScope.ServiceProvider.GetRequiredService<IValidationPolicy<Customer>>();
+
+            _validEntity = CustomersValidationDataFactory.CreateValid();
+            _invalidExternalIdEntity = CustomersValidationDataFactory.CreateInvalidExternalId();
+            _allInvalidEntity = CustomersValidationDataFactory.CreateAllInvalid();
+        }
+
+        [Benchmark(Baseline = true)]
+        public async Task Validate_Success_HappyPath()
+        {
+            await _policy.Validate(_validEntity);
+        }
+
+        [Benchmark]
+        public async Task Validate_Failure_SingleError()
+        {
+            await _policy.Validate(_invalidExternalIdEntity);
+        }
+
+        [Benchmark]
+        public async Task Validate_Failure_MultipleErrors()
+        {
+            await _policy.Validate(_allInvalidEntity);
+        }
+
+        [GlobalCleanup]
+        public void Cleanup()
+        {
+            _serviceScope.Dispose();
+            if (_serviceProvider is IDisposable disposable)
+            {
+                disposable.Dispose();
+            }
+        }
+    }
+}

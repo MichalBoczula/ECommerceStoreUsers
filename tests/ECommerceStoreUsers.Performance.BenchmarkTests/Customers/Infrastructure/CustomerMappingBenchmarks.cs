ec9f7f5359c6a1c3dc0181bd644b3e3763e826fd
Michal  Boczula <38834900+MichalBoczula@users.noreply.github.com>
2026-05-28 01:02:45 +0200

    Add customer infrastructure mapping benchmarks

---
+++ tests/ECommerceStoreUsers.Performance.BenchmarkTests/Customers/Infrastructure/CustomerMappingBenchmarks.cs
@@ -0,0 +1,34 @@
+﻿using BenchmarkDotNet.Attributes;
+using ECommerceStoreUsers.Domain.AggregatesModel.Customers;
+using ECommerceStoreUsers.Infrastructure.Mapping;
+using ECommerceStoreUsers.Infrastructure.Persistance.Customers;
+using ECommerceStoreUsers.Performance.BenchmarkTests.Customers.Infrastructure.Common;
+
+namespace ECommerceStoreUsers.Performance.BenchmarkTests.Customers.Infrastructure
+{
+    [MemoryDiagnoser]
+    public class CustomerMappingBenchmarks
+    {
+        private CustomerDocument _document = null!;
+        private Customer _domain = null!;
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            _document = CustomerDocumentBenchmarkDataFactory.Create();
+            _domain = CustomerMapping.MapToDomain(_document);
+        }
+
+        [Benchmark]
+        public Customer MapDocumentToDomain()
+        {
+            return CustomerMapping.MapToDomain(_document);
+        }
+
+        [Benchmark]
+        public object MapDomainToDocument()
+        {
+            return CustomerMapping.MapToDocument(_domain);
+        }
+    }
+}

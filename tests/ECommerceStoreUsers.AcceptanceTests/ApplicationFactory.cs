dbdb83f7a1694528642882f319002f7ec1545a2c
Michal Boczula <michal.boczula0101@gmail.com>
2026-05-30 23:49:39 +0200

    Added config for mongo

--- tests/ECommerceStoreUsers.AcceptanceTests/ApplicationFactory.cs
+++ tests/ECommerceStoreUsers.AcceptanceTests/ApplicationFactory.cs
@@ -1,18 +1,47 @@
 ﻿using ECommerceStoreUsers.API;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Testcontainers.MongoDb;
 
 namespace ECommerceStoreUsers.AcceptanceTests
 {
     public class ApplicationFactory : WebApplicationFactory<Program>, IAsyncLifetime
     {
-        public Task InitializeAsync()
+        private const string Database = "IntegrationTestDb";
+        private const string Username = "root";
+        private const string Password = "yourStrong(!)Password";
+
+        private readonly MongoDbContainer _mongoContainer;
+        private string _connectionString = string.Empty;
+
+        public ApplicationFactory()
         {
-            throw new NotImplementedException();
+            _mongoContainer = new MongoDbBuilder("mongo:8.0")
+                .WithUsername(Username)
+                .WithPassword(Password)
+                .Build();
         }
 
-        Task IAsyncLifetime.DisposeAsync()
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
         {
-            throw new NotImplementedException();
+            builder.UseEnvironment("Testing");
+            builder.UseSetting("MongoDbSettings:ConnectionString", _connectionString);
+            builder.UseSetting("MongoDbSettings:DatabaseName", Database);
+        }
+
+        public async Task InitializeAsync()
+        {
+            await _mongoContainer.StartAsync();
+
+            var host = _mongoContainer.Hostname;
+            var port = _mongoContainer.GetMappedPublicPort(27017);
+
+            _connectionString = $"mongodb://{Username}:{Password}@{host}:{port}/{Database}?authSource=admin";
+        }
+
+        public new async Task DisposeAsync()
+        {
+            await _mongoContainer.DisposeAsync();
         }
     }
 }

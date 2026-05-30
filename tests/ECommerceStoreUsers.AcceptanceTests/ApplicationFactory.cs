896866ba1dc36e58ae3bc0d3b97deb608fd82753
Michal Boczula <michal.boczula0101@gmail.com>
2026-05-30 23:45:25 +0200

    Added Application Factory

---
+++ tests/ECommerceStoreUsers.AcceptanceTests/ApplicationFactory.cs
@@ -0,0 +1,18 @@
+﻿using ECommerceStoreUsers.API;
+using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace ECommerceStoreUsers.AcceptanceTests
+{
+    public class ApplicationFactory : WebApplicationFactory<Program>, IAsyncLifetime
+    {
+        public Task InitializeAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        Task IAsyncLifetime.DisposeAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

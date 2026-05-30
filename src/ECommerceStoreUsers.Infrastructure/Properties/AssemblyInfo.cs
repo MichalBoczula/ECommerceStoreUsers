d9d340d411ea3a036cdd778ed2a97ccf9b52e492
Michal Boczula <michal.boczula0101@gmail.com>
2026-05-30 23:55:35 +0200

    Implemented set up for mongo db

--- src/ECommerceStoreUsers.Infrastructure/Properties/AssemblyInfo.cs
+++ src/ECommerceStoreUsers.Infrastructure/Properties/AssemblyInfo.cs
@@ -2,3 +2,4 @@ using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("ECommerceStoreUsers.Infrastructure.UnitTests")]
 [assembly: InternalsVisibleTo("ECommerceStoreUsers.Performance.BenchmarkTests")]
+[assembly: InternalsVisibleTo("ECommerceStoreUsers.AcceptanceTests")]
--- tests/ECommerceStoreUsers.AcceptanceTests/ApplicationFactory.cs
+++ tests/ECommerceStoreUsers.AcceptanceTests/ApplicationFactory.cs
@@ -1,15 +1,20 @@
-﻿using ECommerceStoreUsers.API;
+﻿using System.Reflection;
+using ECommerceStoreUsers.API;
+using ECommerceStoreUsers.Infrastructure.Context;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 using Testcontainers.MongoDb;
 
 namespace ECommerceStoreUsers.AcceptanceTests
 {
     public class ApplicationFactory : WebApplicationFactory<Program>, IAsyncLifetime
     {
-        private const string Database = "IntegrationTestDb";
-        private const string Username = "root";
-        private const string Password = "yourStrong(!)Password";
+        private const string Database = "ecommerce-store-users-db-test";
+        private const string Username = "admin";
+        private const string Password = "admin123";
 
         private readonly MongoDbContainer _mongoContainer;
         private string _connectionString = string.Empty;
@@ -25,8 +30,26 @@ namespace ECommerceStoreUsers.AcceptanceTests
         protected override void ConfigureWebHost(IWebHostBuilder builder)
         {
             builder.UseEnvironment("Testing");
-            builder.UseSetting("MongoDbSettings:ConnectionString", _connectionString);
-            builder.UseSetting("MongoDbSettings:DatabaseName", Database);
+
+            builder.ConfigureAppConfiguration((_, config) =>
+            {
+                var overrides = new Dictionary<string, string?>
+                {
+                    ["MongoDbSettings:ConnectionString"] = _connectionString,
+                    ["MongoDbSettings:DatabaseName"] = Database,
+                    ["MongoDbSettings:CustomerCollectionName"] = "customers",
+                    ["MongoDbSettings:CustomerHistoryCollectionName"] = "customer-history",
+                    ["MongoDbSettings:AdminCollectionName"] = "admins"
+                };
+
+                config.AddInMemoryCollection(overrides);
+            });
+
+            builder.ConfigureServices(services =>
+            {
+                services.RemoveAll<MongoDbContext>();
+                services.AddSingleton<MongoDbContext>();
+            });
         }
 
         public async Task InitializeAsync()
@@ -36,7 +59,27 @@ namespace ECommerceStoreUsers.AcceptanceTests
             var host = _mongoContainer.Hostname;
             var port = _mongoContainer.GetMappedPublicPort(27017);
 
-            _connectionString = $"mongodb://{Username}:{Password}@{host}:{port}/{Database}?authSource=admin";
+            _connectionString = $"mongodb://{Username}:{Password}@{host}:{port}/?authSource=admin";
+
+            using var scope = Services.CreateScope();
+
+            var infrastructureAssembly = Assembly.GetAssembly(typeof(MongoDbContext))
+                ?? throw new InvalidOperationException("Could not locate Infrastructure assembly.");
+
+            var initializerType = infrastructureAssembly.GetType("ECommerceStoreUsers.Infrastructure.Configuration.MongoInitializer")
+                ?? throw new InvalidOperationException("Could not resolve MongoInitializer type.");
+
+            var initializer = ActivatorUtilities.CreateInstance(scope.ServiceProvider, initializerType);
+
+            var initializeMethod = initializerType.GetMethod("InitializeAsync")
+                ?? throw new InvalidOperationException("Could not resolve InitializeAsync method.");
+
+            var initializationTask = (Task?)initializeMethod.Invoke(initializer, new object?[] { default(CancellationToken) });
+
+            if (initializationTask is not null)
+            {
+                await initializationTask;
+            }
         }
 
         public new async Task DisposeAsync()

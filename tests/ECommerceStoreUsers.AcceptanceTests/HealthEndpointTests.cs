b252014129db2b783b59bfb916f31b7eb96a0ef0
Michal Boczula <michal.boczula0101@gmail.com>
2026-05-31 23:38:55 +0200

    Added test

---
+++ tests/ECommerceStoreUsers.AcceptanceTests/HealthEndpointTests.cs
@@ -0,0 +1,22 @@
+﻿namespace ECommerceStoreUsers.AcceptanceTests
+{
+    public class HealthEndpointTests : IClassFixture<ApplicationFactory>
+    {
+        private readonly ApplicationFactory _factory;
+
+        public HealthEndpointTests(ApplicationFactory factory)
+        {
+            _factory = factory;
+        }
+
+        [Fact]
+        public async Task GetHealth_ShouldReturnOk()
+        {
+            using var client = _factory.CreateClient();
+
+            var response = await client.GetAsync("/health");
+
+            response.EnsureSuccessStatusCode();
+        }
+    }
+}

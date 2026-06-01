6eaf039268d22865e7e0b83ba22ca54af6165ef9
Michal Boczula <michal.boczula0101@gmail.com>
2026-06-01 22:49:02 +0200

    Added tests set up

---
+++ tests/ECommerceStoreUsers.AcceptanceTests/Features/Common/ScenarioApiContext.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json;
+
+namespace ECommerceStoreUsers.AcceptanceTests.Features.Common
+{
+    public sealed class ScenarioApiContext
+    {
+        public HttpClient HttpClient { get; set; } = default!;
+        public HttpResponseMessage? Response { get; set; }
+
+        public JsonSerializerOptions JsonOptions { get; } = new()
+        {
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            PropertyNameCaseInsensitive = true
+        };
+    }
+}
---
+++ tests/ECommerceStoreUsers.AcceptanceTests/Features/Common/TestHooks.cs
@@ -0,0 +1,35 @@
+﻿using Reqnroll;
+
+namespace ECommerceStoreUsers.AcceptanceTests.Features.Common
+{
+    [Binding]
+    public sealed class TestHooks
+    {
+        private readonly ScenarioApiContext _apiContext;
+        private ApplicationFactory? _factory;
+
+        public TestHooks(ScenarioApiContext apiContext)
+        {
+            _apiContext = apiContext;
+        }
+
+        [BeforeScenario]
+        public async Task BeforeScenario()
+        {
+            _factory = new ApplicationFactory();
+            await _factory.InitializeAsync();
+            _apiContext.HttpClient = _factory.CreateClient();
+        }
+
+        [AfterScenario]
+        public async Task AfterScenario()
+        {
+            _apiContext.HttpClient?.Dispose();
+
+            if (_factory is not null)
+            {
+                await _factory.DisposeAsync();
+            }
+        }
+    }
+}
